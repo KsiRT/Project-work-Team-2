72936b89b7ac76964e7893910a5500f35d2faa19
Olga-Olga <olga_l@ukr.net>
2023-05-27 15:57:34 +0300

    media tablet-desktop

--- src/js/modal.js
+++ src/js/modal.js
@@ -10,7 +10,7 @@
 
   function toggleModal() {
     refs.modal.classList.toggle("hidden");
-    // document.body.classList.toggle('no-scroll');
+    document.body.classList.toggle('no-scroll');
   }
 })();
 
