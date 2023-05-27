61728aeb3b96b863f84cd7c273729560b0dcad7b
Oleh Andrijchuk <107946653+OAndrijchuk@users.noreply.github.com>
2023-05-27 19:36:29 +0300

    our-products done

--- src/main.js
+++ src/main.js
@@ -1,4 +1,49 @@
 import './js/modal-reviews';
-import './js/swiper-bundle';
-import './js/slider-settings';
+// import './js/swiper-bundle';
+// import './js/slider-settings';
 import './js/mob-menu-js';
+// ====================slider-settings============================
+const sliderReview = new Swiper('.reviews-cards-container', {
+  // slidesPerView: 1,
+  spaceBetween: 16,
+  loop: true,
+  mousewheel: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  grabCursor: true,
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1200: {
+      slidesPerView: 3,
+      spaceBetween: 28,
+    },
+  },
+});
+const sliderProd = new Swiper('.prod-list-container', {
+  slidesPerView: 1,
+  spaceBetween: 18,
+  mousewheel: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2.6,
+    },
+    1200: {
+      slidesPerView: 4,
+    },
+  },
+});
+// ==============================================================
